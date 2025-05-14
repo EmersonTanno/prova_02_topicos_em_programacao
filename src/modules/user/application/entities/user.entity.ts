de0abb2b125afdf1c9f1e97183f995ba8c7348f8
kaikdorvalo <kaikdorvalo@gmail.com>
2025-05-13 21:35:37 -0300

    :sparkles: feat(user): add user entity

--- src/modules/user/application/entities/user.entity.ts
+++ src/modules/user/application/entities/user.entity.ts
@@ -1,6 +1,23 @@
-import { Entity } from "typeorm";
+import { Column, CreateDateColumn, Entity, PrimaryColumn, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
 @Entity()
 export class UserEntity {
 
+    @PrimaryGeneratedColumn("uuid")
+    id: string
+
+    @Column({ nullable: false })
+    name: string
+
+    @Column({ nullable: false })
+    email: string
+
+    @Column({ nullable: false })
+    password: string
+
+    @CreateDateColumn()
+    createdAt: Date
+
+    @UpdateDateColumn()
+    updatedAt: Date
 }
