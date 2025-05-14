520023da6cfbb0dc7a9ea57fabf0f9871eeacd49
kaikdorvalo <kaikdorvalo@gmail.com>
2025-05-13 22:20:14 -0300

    :sparkles: feat(jwt): add jwt module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './modules/database/database.module';
 import { UserModule } from './modules/user/user.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
+    JwtModule.register({
+      global: true,
+      secret: 'segredo',
+      signOptions: { expiresIn: '60s' },
+    }),
     UserModule
   ],
   controllers: [AppController],
