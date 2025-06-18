a5d89263667cfae395dbefb6be25a3efb31a7119
Emerson Tanno <tannobrizotto@gmail.com>
2025-06-17 22:05:53 -0300

    update: user.module: adding new providers

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -5,6 +5,10 @@ import { CreateUserUserCase } from "./application/use-cases/create-user.use-case
 import { UserRepository } from "./infrastructure/repositories/user.repository";
 import { HashPasswordUseCase } from "./application/use-cases/hash-password.use-case";
 import { UserService } from "./application/services/user.service";
+import { DeleteUserUseCase } from "./application/use-cases/delete-user.use-case";
+import { UpdateUserProfileUseCase } from "./application/use-cases/update-user.use-case";
+import { GetUserProfileUseCase } from "./application/use-cases/get-user.use-case";
+import { GetAllUserProfileUseCase } from "./application/use-cases/getAll-user.use-case";
 
 @Module({
     imports: [
@@ -21,6 +25,10 @@ import { UserService } from "./application/services/user.service";
         CreateUserUserCase,
         HashPasswordUseCase,
         UserService,
+        DeleteUserUseCase,
+        UpdateUserProfileUseCase,
+        GetUserProfileUseCase,
+        GetAllUserProfileUseCase
     ]
 })
 export class UserModule { }
